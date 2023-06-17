68f618669f7b779b12fd8e9547cbe2c986adbb12
Sayan <sayanmunshi36@gmail.com>
2023-06-17 18:15:16 +0530

    update user controller, update user model, add generateToken function

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,41 +1,45 @@
+import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/user.model.js";
+
 // Method       POST
 // Route       /register
 // Permission  Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
     success: true,
     message: "Register Route",
   });
-};
+});
 
 // Method       POST
 // Route       /login
 // Permission  Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.status(200).json({
     success: true,
     message: "Login Route",
   });
-};
+});
 
 // Method       GET
 // Route       /logout
 // Permission  Public
-const logoutUser = (req, res) => {
+const logoutUser = asyncHandler(async (req, res) => {
   res.status(200).json({
     success: true,
     message: "Logout Route",
   });
-};
+});
 
 // Method       GET
 // Route       /me
 // Permission  Protected
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.status(200).json({
     success: true,
     message: "My Profile",
   });
-};
+});
 
 export { registerUser, loginUser, logoutUser, getMe };
--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -24,11 +25,48 @@ const userSchema = mongoose.Schema(
       type: String,
       maxLength: 200,
     },
+    profile_pic: {
+      type: String,
+      default: "Link to default image",
+    },
+    products_count: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "products",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password using bcrypt
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
