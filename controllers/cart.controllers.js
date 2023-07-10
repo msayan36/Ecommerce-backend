ef9a484c6d7f5be0868684b7537fb452381d8f88
Sayan <sayanmunshi36@gmail.com>
2023-07-10 07:37:20 +0530

    Cart Route

--- controllers/cart.controllers.js
+++ controllers/cart.controllers.js
@@ -1,24 +1,28 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/user.model.js";
+import Product from "../models/product.model.js";
 
 // @Desc        Add to Cart
 // @Route       POST /api/v1/cart
 // @Permission  Protected
 const addToCart = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    status: 200,
-    msg: "Add to Cart",
-  });
+  const pdtId = req.query.product;
+  const user = await User.findById(req.user._id);
+
+  user.cart.push(pdtId);
+  const newUser = await user.save();
+
+  res.status(200).json(newUser);
 });
 
 // @Desc        GET Cart Items
 // @Route       GET /api/v1/cart
 // @Permission  Protected
 const getCart = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    status: 200,
-    msg: "Get Cart Items",
-  });
+  const { cart } = await User.findById(req.user._id);
+  const products = await Product.find({ _id: { $in: cart } });
+
+  res.status(200).json(products);
 });
 
 export { addToCart, getCart };
